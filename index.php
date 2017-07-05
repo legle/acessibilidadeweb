58d32217514b38e252e281a0daf4500092f8c47d
Leonardo Gleison Ferreira <leonardo.gleison@gmail.com>
2017-07-05 13:37:44 -0300

    Modificação de htm para php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>Exemplos de Componentes HTML5 Acessíveis</title>
+	</head>
+	<body>
+		<div>
+			<h1>Acessibilidade Web para Pessoas com Defici&ecirc;ncia Visual</h1>
+			<p>
+				Esta simples p&aacute;gina é uma modesta contribui&ccedil;ão para a comunidade de desenvolvedores web, que tem como objetivo contribuir para a evolu&ccedil;&Atilde;o da acessibilidade
+				na web para às pessoas com defici&ecirc;ncia, oferecendo exemplos testados por elas de como usar recursos do HTML5, WAI-ARIA e JQuery, além de outras tecnologias
+				que eventualmente surgirem ao longo do tempo.
+			</p>
+			<p>
+				A ideia é desenvolver e testar componentes, técnicas e recursos web e disponibilizar para que outros desenvolvedores saibam como fazer, por isso, se você
+				desenvolveu algo que funciona, por favor, contribua com este projeto!
+			</p>
+			<h2>Exemplos</h2>
+			<ul>
+				<li><a href="/tabpanel/index.htm">Painel com várias guias</a></li>
+			</ul>
+			<h2>Contato</h2>
+			<ul>
+				<li><a href="http://www.twitter.com/leonardo_gf">Twitter @leonardo_gf</a></li>
+				<li><a href="http://www.github.com/legle">Github @legle</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
